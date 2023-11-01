9f59b8415f65f3c90e469d493e42df7f34385fb4
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-01 12:55:55 +0530

    Update EmployeeService.javas

--- Ems1/src/main/java/com/example2/Service/EmployeeService.java
+++ Ems1/src/main/java/com/example2/Service/EmployeeService.java
@@ -1,21 +1,17 @@
 package com.example2.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import com.example2.Entity.Employee;
-
-
 
 public interface EmployeeService 
 {
 	
-	List<Employee> getAllEmployees();
-	
-	Optional<Employee> getEmployeeById(int id);
+	List<Employee> getEmployees();
+	Employee getEmployeeById(int id);
 	Employee updateEmployee(int id,Employee employee);
 	String deleteEmployee(int id);
-	
-	
+	Employee addEmployee(Employee employee);
+	String assignDepartmentToEmployee(int did, int eid);
+
 }
